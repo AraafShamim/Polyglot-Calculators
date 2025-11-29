6e873a7048b85544775c598c968b17d8b11bc37f
ramakrihsna <2003ramakrishna@gmail.com>
2025-11-29 19:57:24 +0530

    python functions to convert distance from one to unit to another

---
+++ Playground/MALLAVENKATASIVARAMAKRISHNA/main.py
@@ -0,0 +1,18 @@
+def miles_to_kilometers(miles):
+    km = miles * 0.609344
+    return km
+def kilometers_to_miles(km):
+    miles = km / 0.609344
+    retun miles
+def meters_to_feet(meters):
+    feet = meters * 3.28084
+    return feet
+def feet_to_meters(feet):
+    meters = feet / 3.28084
+    return meters
+def centimeters_to_inches(centimeters):
+    inches = centimeters * 0.393701
+    return inches
+def inches_to_centimeters(inches):
+    centimeters = inches / 0.393701
+    return cntimeters
